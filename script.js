1556ec17b94bc190018ad9d02923973137f9f38e
Adeel Hussain <adeelh117@gmail.com>
2020-10-13 11:04:48 -0700

    Fixed most functionality

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 const wrapper = document.getElementById("wrapper");
 
-function evaluate(operator, value, value2) 
+function evaluate(operator, value, value2)
 {
     if (operator == "+")
-    {
         return +value + +value2;
-    }
-    if (operator == "-")
-    {
+    else if (operator == "-")
         return +value - +value2;
-    }
-    if (operator == "/")
-    {
+    else if (operator == "/")
         return +value / +value2;
-    }
-    if (operator == "*")
-    {
+    else if (operator == "*")
         return +value * +value2;
-    }
+    
 
     return 0;
 }
-function validate(content) {
-    if (isNaN(display.textContent) || display.textContent == '0')
-        return false;
-    return true;
-}
-function isOperator(content) {
+function isOperator(content) 
+{
     if (content == '+' || content == '-' || content == '/' || content == "*")
         return true;
     return false;
@@ -34,60 +23,98 @@ function isOperator(content) {
 
 wrapper.addEventListener('click', (event) => 
 {
+    // check if clicking background
     if (event.target.value == undefined) 
         return;
-    
 
-    console.log(event.target.value); // <
+    let curr = event.target.value;
 
-    const display = document.getElementById("display");
-    let decFlag = false;
-
-    switch (event.target.value)
+    switch (curr)
     {
         case "+":
-            if (!validate(event.target.value))
-                break;
-            nums.unshift(display.textContent);
-            console.table(nums)
-            display.textContent = '+';
-            break;
         case "-":
-            if (!validate(event.target.value))
+        case "*":
+        case "/":
+            if (nums.length == 0 && lastNum != null)
+            {
+                nums.unshift(lastNum);
+                nums.unshift(event.target.id);
+
+                document.getElementById(event.target.id).classList.add("selected");
+
+                lastNum = null;
+            }
+            else if (nums.length == 1)
+            {
+                decFlag = false; 
+                nums.unshift(event.target.id);
+                document.getElementById(event.target.id).classList.add("selected");
+            }
+            else if (isOperator(nums[0]) && !isOperator(event.target.value))
+            {
+                decFlag = false;
+                document.getElementById(nums[0]).classList.remove("selected");
+                let result = evaluate(nums.shift(), nums.shift(), lastNum);
+                nums.unshift(result);
+                nums.unshift(event.target.id);
+                document.getElementById(nums[0]).classList.add("selected");
+                display.textContent = result;
+                lastNum = null;
+            }
+            console.log(nums);
+            break;
+        case "=":
+            if (nums.length != 2)
                 break;
-            nums.unshift(display.textContent);
-            console.table(nums)
-            display.textContent = '-';
+            else if (lastNum != null)
+            {
+                decFlag = false;
+                document.getElementById(nums[0]).classList.remove("selected");
+                let result = evaluate(nums.shift(), nums.shift(), lastNum);
+                nums.unshift(result);
+                display.textContent = result;
+                lastNum = null;
+            }
             break;
         case "AC":
-            while (nums.pop()) { }
+            while (nums.pop()) {}
             display.textContent = '0';
-            console.table(nums);
+            lastNum = null;
             break;
         case "B":
-            if (isOperator(display.textContent))
-                break;
-            display.textContent = display.textContent.substr(0, display.textContent.length - 1);
+            let result = display.textContent.substr(0, display.textContent.length - 1)
+            display.textContent = result;
+            if (!isNaN(nums[0]))
+            {
+                nums.shift();
+                nums.unshift(result);
+            }
             if (display.textContent == "")
+            {
                 display.textContent = '0';
+            }
+            lastNum = display.textContent.substr(0, display.textContent.length - 1);
+            break;
+        case ".":
+            if (decFlag || lastNum == null)
+                break;
+            
+            lastNum += curr;
+            display.textContent = lastNum;
+            decFlag = true;
             break;
         default:
-            if (isOperator(display.textContent))
-            {
-                display.textContent = evaluate(display.textContent, event.target.value, nums.pop());
-            }
-            else if (display.innerHTML == '0')
-                display.textContent = event.target.value;
-            else if (display.innerHTML >= 9999999999999999)
-                break;
-            else 
-            {
-                let textNode = document.createTextNode(event.target.value);
-                display.appendChild(textNode);
-            }
-
+            if (lastNum == null)
+                lastNum = curr;
+            else
+                lastNum += curr;
+            
+            display.textContent = lastNum;
             break;
     }
 });
 
 let nums = [];
+let lastNum = null;
+const display = document.getElementById("display");
+let decFlag = false;
